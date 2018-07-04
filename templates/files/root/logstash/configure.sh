6dad54fa254b87ed332478630b893eae5ccaac77
rostskadat <rostskadat@gmail.com>
2018-07-04 16:00:02 +0200

    Adding missing logstash configuration scripts

--- templates/files/root/logstash/configure.sh
+++ templates/files/root/logstash/configure.sh
@@ -4,29 +4,39 @@
 #
 # DESCRIPTION: This script will 
 #
-logGroup="$1"
-[ -z "$logGroup" ] && echo "Invalid logGroup" && exit 1
+LogGroup="$1"
+LogstashInputConfig="$2"
+[ -z "${LogGroup}" ] && echo "Invalid LogGroup" && exit 1
+[ -z "${LogstashInputConfig}" ] && echo "Invalid LogstashInputConfig" && exit 1
 
-echo "Configuring Logstash..."
-# Forcing to create init.d file
-/usr/share/logstash/bin/system-install /etc/logstash/startup.options sysv
 # Adding logs to awslog
 cat >> /etc/awslogs/awslogs.conf <<EOF
 [/var/log/logstash-stdout.log]
 file = /var/log/logstash-stdout.log
 log_stream_name = {instance_id}/var/log/logstash-stdout.log
-log_group_name = ${logGroup}
+log_group_name = ${LogGroup}
 [/var/log/logstash-stderr.log]
 file = /var/log/logstash-stderr.log
 log_stream_name = {instance_id}/var/log/logstash-stderr.log
-log_group_name = ${logGroup}
+log_group_name = ${LogGroup}
 [/var/log/logstash/grok_failures.txt]
 file = /var/log/logstash/grok_failures.txt
 log_stream_name = {instance_id}/var/log/logstash/grok_failures.txt
-log_group_name = ${logGroup}
+log_group_name = ${LogGroup}
 EOF
-# Extracting GeoLite2-City DB
+
+echo "Configuring Logstash init script..."
+/usr/share/logstash/bin/system-install /etc/logstash/startup.options sysv
+
+echo "Extracting GeoLite2-City DB..."
 geodb=$(tar -tvf /tmp/GeoLite2-City.tar.gz | grep 'mmdb' | sed -E 's/.* ([^ ]+)$/\1/')
 [ -d /usr/share/logstash/geolite ] || mkdir -p /usr/share/logstash/geolite
 tar -xvzf /tmp/GeoLite2-City.tar.gz -C /usr/share/logstash/geolite $geodb
 mv /usr/share/logstash/geolite/$geodb /usr/share/logstash/geolite/. 
+
+echo "Configuring input..."
+aws s3 cp ${LogstashInputConfig} /etc/logstash/conf.d/01-input.conf
+
+echo "Configuring IPv4..."
+echo "-Djava.net.preferIPv4Stack=true" >> /etc/logstash/jvm.options
+sed -ibckp -E "s/.*http.host.*/http.host: '0.0.0.0'/" /etc/logstash/logstash.yml
--- templates/files/root/logstash/s3_sync_logs.sh
+++ templates/files/root/logstash/s3_sync_logs.sh
@@ -4,13 +4,17 @@
 #
 # DESCRIPTION: This script will 
 #
-s3ImportBucketName="$1"
-[ -z "$s3ImportBucketName" ] && echo "Invalid s3ImportBucketName" && exit 1
+S3LogsIncludeFilters="{{S3LogsIncludeFilters}}"
+[ -z "${S3LogsIncludeFilters}" ] && echo "Invalid S3LogsIncludeFilters" && exit 1
 
 DST_DIR=/var/log/imported
 [ -d $DST_DIR ] || mkdir -p $DST_DIR
 chmod go+rx $DST_DIR
-aws s3 sync s3://$s3ImportBucketName/ $DST_DIR/ --exclude "*" --include "*SSL_access_$(date +"%Y-%m-%d")-00_00_00.log"
-aws s3 sync s3://$s3ImportBucketName/ $DST_DIR/ --exclude "*" --include "*ArcQueriesTrace.log"
-aws s3 sync s3://$s3ImportBucketName/ $DST_DIR/ --exclude "*" --include "*api-backend.log"
+
+IFS=','
+for include_filter in $(echo -n "${S3LogsIncludeFilters}"); do
+    aws s3 sync s3://${S3LogsBucketName}/ $DST_DIR/ --exclude "*" --include "${S3LogsIncludeFilters}"
+done
 find $DST_DIR/ -name SSL_access_$(date --date="-1 day" +"%Y-%m-%d")-00_00_00.log -exec rm {} \;
+
+
