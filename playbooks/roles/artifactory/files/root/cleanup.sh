af6cafd27078be1d6a79d6744199590144f71a03
rostskadat <rostskadat@gmail.com>
2018-07-18 14:03:04 +0200

    Adding simple cleanup script

--- playbooks/roles/artifactory/files/root/cleanup.sh
+++ playbooks/roles/artifactory/files/root/cleanup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Are you sure you want to cleanup and start over?"
-echo "This will erase evrything and is not recoverable!"
+echo "This will erase everything and is not recoverable!"
 echo ""
 echo "Press Enter to continue, Ctrl-C to quit"
 read line
 
-ansible-playbook /root/playbooks/artifactory.yaml --extra-vars=@/root/playbooks/group_vars/stack.yaml --tags "cleanup"
+ansible-playbook /root/playbooks/cleanup.yaml --extra-vars=@/root/playbooks/group_vars/stack.yaml
