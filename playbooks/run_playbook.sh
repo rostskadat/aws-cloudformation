181f608f25cdb77c76fc0a896ee5eeed7c856faf
rostskadat <rostskadat@gmail.com>
2018-07-27 11:34:11 +0200

    Adding possibility to send signal from the run_playbook script

--- playbooks/run_playbook.sh
+++ playbooks/run_playbook.sh
@@ -3,4 +3,20 @@
 # I redirect all the logs to a monitorized file
 exec > >(tee -a "/var/log/playbook.log") 2> >(tee -a "/var/log/playbook.log")
 
-ansible-playbook --extra-vars=@/root/playbooks/group_vars/stack.yaml --skip-tags "cleanup" "$1"
+playbook="$1"
+Region="$2"
+StackName="$3"
+[ -z "$playbook" ] && echo "Invalid playbook" && exit 1
+
+#[ -z "$Region" ] && echo "Invalid Region" && exit 1
+#[ -z "$StackName" ] && echo "Invalid StackName" && exit 1
+
+
+ansible-playbook --extra-vars=@/root/playbooks/group_vars/stack.yaml --skip-tags "cleanup" "$playbook"
+rc=$?
+
+if [ ! -z "$Region" ] && [ ! -z "$StackName" ]; then
+    echo "Signaling as requested ($rc)"
+    /root/signal_asg.sh $rc "$Region" "$StackName"
+fi
+exit $rc
