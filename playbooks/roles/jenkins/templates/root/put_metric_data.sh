b9583b68afdbfcee9a830b3df08a2d811f41339b
rostskadat <rostskadat@gmail.com>
2018-07-27 11:33:30 +0200

    Use external script to avoid leaking password in logs

---
+++ playbooks/roles/jenkins/templates/root/put_metric_data.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+MetricName="$1"
+
+[ -z "$MetricName" ] && echo "Invalid MetricName" && exit 1
+
+case "$MetricName" in
+    BuildActive) 
+        aws --region {{Region}} cloudwatch put-metric-data --namespace {{StackName}} --metric-name BuildActive --value $(curl -s -m 60 -u 'admin:{{JenkinsAdminPassword}}' 'http://localhost:8080/computer/api/json' | jq -r '.busyExecutors') --unit Count ;;
+    BuildActive) 
+        aws --region {{Region}} cloudwatch put-metric-data --namespace {{StackName}} --metric-name BuildQueue --value $(curl -s -m 60 -u 'admin:{{JenkinsAdminPassword}}' 'http://localhost:8080/jqs-monitoring/api/json' | jq '.buildQueue.numberOfJobs') --unit Count ;;
+    *) echo "Invalid MetricName: Must be either BuildActive or BuildQueue" ;;
+esac
