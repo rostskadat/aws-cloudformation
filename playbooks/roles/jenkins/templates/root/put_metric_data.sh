3c02a5d7665de7a0f12f821904cc89f8b456619e
rostskadat <rostskadat@gmail.com>
2018-07-27 17:46:23 +0200

    Use localhost to avoid problem

--- playbooks/roles/jenkins/templates/root/put_metric_data.sh
+++ playbooks/roles/jenkins/templates/root/put_metric_data.sh
@@ -6,8 +6,8 @@ MetricName="$1"
 
 case "$MetricName" in
     BuildActive) 
-        aws --region {{Region}} cloudwatch put-metric-data --namespace {{StackName}} --metric-name BuildActive --value $(curl -s -m 60 -u 'admin:{{JenkinsAdminPassword}}' 'http://{{JenkinsDNSName}}/computer/api/json' | jq -r '.busyExecutors') --unit Count ;;
+        aws --region {{Region}} cloudwatch put-metric-data --namespace {{StackName}} --metric-name BuildActive --value $(curl -s -m 60 -u 'admin:{{JenkinsAdminPassword}}' 'http://localhost:8080/computer/api/json' | jq -r '.busyExecutors') --unit Count ;;
     BuildActive) 
-        aws --region {{Region}} cloudwatch put-metric-data --namespace {{StackName}} --metric-name BuildQueue --value $(curl -s -m 60 -u 'admin:{{JenkinsAdminPassword}}' 'http://{{JenkinsDNSName}}/jqs-monitoring/api/json' | jq '.buildQueue.numberOfJobs') --unit Count ;;
+        aws --region {{Region}} cloudwatch put-metric-data --namespace {{StackName}} --metric-name BuildQueue --value $(curl -s -m 60 -u 'admin:{{JenkinsAdminPassword}}' 'http://localhost:8080/jqs-monitoring/api/json' | jq '.buildQueue.numberOfJobs') --unit Count ;;
     *) echo "Invalid MetricName: Must be either BuildActive or BuildQueue" ;;
 esac
