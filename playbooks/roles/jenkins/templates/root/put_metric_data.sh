f48f93fad60ba2514cec5f81538794fd610b842a
rostskadat <rostskadat@gmail.com>
2018-07-31 15:12:45 +0200

    No-fast forward

--- playbooks/roles/jenkins/templates/root/put_metric_data.sh
+++ playbooks/roles/jenkins/templates/root/put_metric_data.sh
@@ -4,10 +4,14 @@ MetricName="$1"
 
 [ -z "$MetricName" ] && echo "Invalid MetricName" && exit 1
 
+credentials="admin:{{JenkinsAdminPassword}}"
+
 case "$MetricName" in
-    BuildActive) 
-        aws --region {{Region}} cloudwatch put-metric-data --namespace {{StackName}} --metric-name BuildActive --value $(curl -s -m 60 -u 'admin:{{JenkinsAdminPassword}}' 'http://localhost:8080/computer/api/json' | jq -r '.busyExecutors') --unit Count ;;
-    BuildQueue) 
-        aws --region {{Region}} cloudwatch put-metric-data --namespace {{StackName}} --metric-name BuildQueue --value $(curl -s -m 60 -u 'admin:{{JenkinsAdminPassword}}' 'http://localhost:8080/jqs-monitoring/api/json' | jq '.buildQueue.numberOfJobs') --unit Count ;;
-    *) echo "Invalid MetricName: Must be either BuildActive or BuildQueue" ;;
+    BuildActive)
+        value=$(curl -s -m 60 -u "$credentials" 'http://localhost:8080/computer/api/json' | jq -r '.busyExecutors') ;;
+    BuildQueue)
+        value=$(curl -s -m 60 -u "$credentials" 'http://localhost:8080/jqs-monitoring/api/json' | jq '.buildQueue.numberOfJobs') ;;
+    *) echo "Invalid MetricName: Must be either BuildActive or BuildQueue" && exit 1;;
 esac
+echo "Add metric $MetricName: $value"
+aws --region "{{Region}}" cloudwatch put-metric-data --namespace "{{StackName}}" --metric-name $MetricName --unit Count --value $value
