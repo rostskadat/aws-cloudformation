e5c7ef4f78ac586eebfbc333d297fb119131e016
JULIEN MASNADA <rostskadat@users.noreply.github.com>
2018-07-31 16:06:50 +0200

    Delete sync.sh

--- sync.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-S3_BUCKET_NAME=cloudformation-eu-west-1-791682668801
-AWS=$(([ -f /usr/local/bin/aws ] && echo -n /usr/local/bin/aws) || echo -n $(which aws))
-
-$AWS s3 sync stacks s3://$S3_BUCKET_NAME/stacks
-$AWS s3 sync playbooks s3://$S3_BUCKET_NAME/playbooks
-echo "Sync OK"
-
