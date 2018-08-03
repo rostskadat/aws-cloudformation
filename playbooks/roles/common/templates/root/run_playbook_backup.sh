5373dd8af2e8c575824d579d14f339b805da3859
rostskadat <rostskadat@gmail.com>
2018-08-03 15:58:49 +0200

    Removing association from each stack and migrate it to VPC

---
+++ playbooks/roles/common/templates/root/run_playbook_backup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# I redirect all the logs to a monitorized file
+exec > >(tee -a "/var/log/playbook.log") 2> >(tee -a "/var/log/playbook.log")
+
+Playbook="{{PlaybookBackup}}"
+
+[ "$Playbook" = "{{PlaybookBackup}}" ] && echo "Invalid Playbook argument" && exit 1
+
+ansible-playbook --extra-vars=@/root/playbooks/group_vars/stack.yaml --skip-tags "cleanup" "$Playbook"
