1bff02348df66893cf8a4dc3bbe98ff6127ecb3b
rostskadat <rostskadat@gmail.com>
2018-08-03 08:41:07 +0200

    Make parameter name more consistent with other scripts

--- playbooks/roles/common/files/root/tag_root_volume.sh
+++ playbooks/roles/common/files/root/tag_root_volume.sh
@@ -7,13 +7,13 @@
 #   It will determine the volume_id attached to the current instance, extract the PLATFROM tag of that instance and
 #   propagate the tag to the volume.
 #
-region="$1"
-stack_name="$2"
-[ -z "$region" ] && echo "Invalid region" && exit 1
-[ -z "$stack_name" ] && echo "Invalid stack_name" && exit 1
+Region="$1"
+StackName="$2"
+[ -z "$Region" ] && echo "Invalid Region" && exit 1
+[ -z "$StackName" ] && echo "Invalid StackName" && exit 1
 echo "Tagging Root volume..."
 instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
-volume_id=$(aws --region $region ec2 describe-volumes --filters "Name=attachment.instance-id,Values=$instance_id" "Name=attachment.device,Values=/dev/xvda" --query "Volumes[0].VolumeId" --output text)
-platform=$(aws --region $region ec2 describe-instances --instance-ids $instance_id --query "Reservations[0].Instances[0].Tags" --output table | grep PLATFORM | cut -d '|' -f 3 | sed -e 's/ //g')
-aws --region $region ec2 create-tags --resources $volume_id --tag "Key=Name,Value=${stack_name}-VolumeRoot"
-aws --region $region ec2 create-tags --resources $volume_id --tag "Key=PLATFORM,Value=$platform"
+volume_id=$(aws --region $Region ec2 describe-volumes --filters "Name=attachment.instance-id,Values=$instance_id" "Name=attachment.device,Values=/dev/xvda" --query "Volumes[0].VolumeId" --output text)
+platform=$(aws --region $Region ec2 describe-instances --instance-ids $instance_id --query "Reservations[0].Instances[0].Tags" --output table | grep PLATFORM | cut -d '|' -f 3 | sed -e 's/ //g')
+aws --region $Region ec2 create-tags --resources $volume_id --tag "Key=Name,Value=${StackName}-VolumeRoot"
+aws --region $Region ec2 create-tags --resources $volume_id --tag "Key=PLATFORM,Value=$platform"
