560ee7b924e93f2fe377af30642d8c2119786910
rostskadat <rostskadat@gmail.com>
2018-09-03 18:14:25 +0200

    Added common values for FunctionGetVariable, and fixed SSM commands

--- playbooks/roles/common/templates/root/run_playbook_install.sh
+++ playbooks/roles/common/templates/root/run_playbook_install.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
-
+#
+# FILE: run_playbook_install.sh
+#
+# DESCRIPTION: This command is in charge of calling the install playbook.
+#   The main automation entry point is accessible through the AWS System Manager console
+#   (https://console.aws.amazon.com/systems-manager/documents). it is meant to ease the 
+#   configuration of an existing instance. Follow these steps to execute this command
+#
+# - Open https://console.aws.amazon.com/systems-manager/run-command/executing-commands
+# - Click on "Run Command"
+# - Filters Documents by Owner: only display document "Owned by me"
+# - Select the "DocumentInstall"
+# - Select the Instance on which to run the command.
+# - Click "Run"   
+#
 # I redirect all the logs to a monitorized file
 exec > >(tee -a "/var/log/playbook.log") 2> >(tee -a "/var/log/playbook.log")
 
 Playbook="{{PlaybookInstall}}"
 
-[ "$Playbook" = "{{PlaybookInstall}}" ] && echo "Invalid Playbook argument" && exit 1
+[ ! -f "$Playbook" ] && echo "Invalid Playbook argument" && exit 1
 
 ansible-playbook --extra-vars=@/root/playbooks/group_vars/stack.yaml --skip-tags "cleanup" "$Playbook"
