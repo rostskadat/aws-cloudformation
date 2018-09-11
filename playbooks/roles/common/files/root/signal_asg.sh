d7e0c398c18408a6bc1c4bd9c9538d9d4b5b7e03
rostskadat <rostskadat@gmail.com>
2018-09-11 12:02:14 +0200

    Make sure the Swarm Launch configuration signal the correct ASG

--- playbooks/roles/common/files/root/signal_asg.sh
+++ playbooks/roles/common/files/root/signal_asg.sh
@@ -5,17 +5,23 @@ Region="$2"
 StackName="$3"
 StackTag="$4"
 AutoScalingGroup="$5"
+IsSubStack="$6"
 [ -z "$ExitCode" ] && echo "Invalid ExitCode" && exit 1
 [ -z "$Region" ] && echo "Invalid Region" && exit 1
 [ -z "$StackName" ] && echo "Invalid StackName" && exit 1
 [ -z "$StackTag" ] && echo "Invalid StackTag" && exit 1
 [ -z "$AutoScalingGroup" ] && echo "Invalid AutoScalingGroup" && exit 1
 
-StackId=$(aws --region $Region cloudformation describe-stacks --stack-name $StackName --query "Stacks[0].StackId" --output text)
-StackNameASG=$(aws --region $Region cloudformation describe-stacks --query "Stacks[?ParentId == '$StackId' && Tags[?Value == '$StackName-$StackTag' ]].StackName" --output text)
-if [ ! -z "$StackNameASG" ]; then
-	echo "Signaling $ExitCode to $AutoScalingGroup (from Stack $StackNameASG)"
-	/opt/aws/bin/cfn-signal -e 0 --region "$Region" --stack "$StackNameASG" --resource "$AutoScalingGroup"
+
+if [ -z "$IsSubStack" ]; then
+	StackId=$(aws --region $Region cloudformation describe-stacks --stack-name $StackName --query "Stacks[0].StackId" --output text)
+	StackNameASG=$(aws --region $Region cloudformation describe-stacks --query "Stacks[?ParentId == '$StackId' && Tags[?Value == '$StackName-$StackTag' ]].StackName" --output text)
+	if [ ! -z "$StackNameASG" ]; then
+		echo "Signaling $ExitCode to $AutoScalingGroup (from Stack $StackNameASG)"
+		/opt/aws/bin/cfn-signal -e 0 --region "$Region" --stack "$StackNameASG" --resource "$AutoScalingGroup"
+	else
+		echo "No Nested Stack with Tag '$StackName-$StackTag' found. Bailing Out!"
+	fi
 else
-	echo "No Nested Stack with Tag '$StackName-ASG' found. Bailing Out!"
-fi
+	/opt/aws/bin/cfn-signal -e 0 --region "$Region" --stack "$StackName" --resource "$AutoScalingGroup"
+fi		
